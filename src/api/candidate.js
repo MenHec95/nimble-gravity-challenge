a4cf3b3d0517211fa331e8f8b97f0384c4d6a118
MenHec95 <hectormendez_95@yahoo.com>
2026-02-22 10:11:37 -0300

    add api services files

---
+++ src/api/candidate.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+export const getCandidateByEmail = async (email) => {
+  const response = await axios.get(`${BASE_URL}/api/candidate/get-by-email`, {
+    params: { email },
+  });
+  return response.data;
+};
+
+export const applyToJob = async ({ uuid, jobId, candidateId, repoUrl }) => {
+  const response = await axios.post(`${BASE_URL}/api/candidate/apply-to-job`, {
+    uuid,
+    jobId,
+    candidateId,
+    repoUrl,
+  });
+  return response.data;
+};
---
+++ src/api/jobs.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+const BASE_URL = import.meta.env.VITE_BASE_URL
+
+export const getJobs = async () => {
+  const response = await axios.get(`${BASE_URL}/api/jobs/get-list`)
+  return response.data
+}
